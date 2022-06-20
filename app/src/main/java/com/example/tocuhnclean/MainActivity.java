297dbbecf65f273c26126cf466cd7a6f55a22827
ramdhansy12 <talladhan2772@gmail.com>
2022-06-21 00:16:20 +0700

    sampai login

--- app/src/main/java/com/example/tocuhnclean/MainActivity.java
+++ app/src/main/java/com/example/tocuhnclean/MainActivity.java
@@ -14,7 +14,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private  static int SPLASH_SCREEN = 5000;
+    private  static int SPLASH_SCREEN = 3000;
 
     //Variable
     Animation topAnim, bottomAnim;
